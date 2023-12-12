86fb40eb49f33376d6f863922a6a0aa03257d308
jerkin23 <145083562+jerkin23@users.noreply.github.com>
2023-12-11 22:48:16 -0600

    descargas comentadas

--- proyecto/src/components/sidebar/sidebar.js
+++ proyecto/src/components/sidebar/sidebar.js
@@ -4,27 +4,37 @@ import './sidebar.css';
 import { IoSearchOutline, IoMenuOutline, IoLogOutOutline,  IoDocumentTextOutline, IoPlanetOutline, IoBookOutline } from 'react-icons/io5';
 import miImagen from './assets/blank-profile-picture-973460_1280-580x580.jpg';
 
-const Sidebar = () => {
-  const [sidebarActive, setSidebarActive] = useState(false);
+// instalar los iconos 
+//npm install ionicons@7.1.0
+// npm install react-icons
 
-  const handleButtonClick = () => {
-    setSidebarActive(!sidebarActive);
+const Sidebar = () => {
+const [sidebarActive, setSidebarActive] = useState(false);
+
+
+const handleButtonClick = () => {
+setSidebarActive(!sidebarActive);
   };
 
-  return (
+return (
 
       <div className='sidebard'>
             <div className={`sidebar ${sidebarActive ? 'active' : ''}`} data-testid="Sidebar">
         <div className='contenido'>
+
+
           <div className='logo'>
             <span className='icono'><IoPlanetOutline /></span>
             <div className='nombre'>
               <span className="titulo">Notes En Linea </span>
             </div>
-            
+
+
           </div>
           <div className='navegacion'>
           <span className='btn' onClick={handleButtonClick}><IoMenuOutline /></span>
+
+
             <ul>
               <li>
                 <a href='#'>
@@ -48,25 +58,32 @@ const Sidebar = () => {
                 <span className='tool'>Manual</span>
               </li>
             </ul>
+
+
             <div className="salir">
               <div className="perfil">
                 <div className="perfil2">
                   <img src={miImagen} alt="" />
+
+
                   <div className="detalles">
                     <div className="cierre">Sign Out</div>
                     <div className="despedida">Thank you for visiting us</div>
                   </div>
+
+
                 </div>
                 <span className='logout'><IoLogOutOutline /></span>
               </div>
             </div>
           </div>
         </div>
-         <div className='home'>
-              <div className='textoPrueba'>home content</div>
-            </div>
+
+
+            <div className='home'>
+          <div className='textoPrueba'>home content</div>
+        </div>
       </div>
-      
     </div>
   );
 };
